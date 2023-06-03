525d5c51f70b2b32d194b1e16e8f7c2f094b84cf
ascended4 <ascended4@gmail.com>
2023-06-03 14:34:54 +0300

    Fix: Added command to delete default selector key

--- 1_K8S_EXAM/EX_05/create-service.sh
+++ 1_K8S_EXAM/EX_05/create-service.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# NOT WORKING
-# There is a problem with auto generate selector with app=messaging-service
 kubectl create service clusterip messaging-service --tcp=6379:6379
 kubectl patch service messaging-service -p '{"spec": {"selector": {"tier" : "msg"}}}'
+kubectl patch service messaging-service --type json -p='[{"op": "remove", "path": "/spec/selector/app"}]'
